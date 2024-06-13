6e808dbf49560d25f34a9527f761cc9f1d2246ce
Chase Kolozsy <chasekolozsy@gmail.com>
2024-06-13 12:22:13 -0600

    updates requirements to include openai, adds import statements to definition generator

---
+++ definition_generator.py
@@ -0,0 +1,12 @@
+import client.src.operations.app_ops
+import client.src.operations.attribute_ops
+import client.src.operations.branch_node_ops
+import client.src.operations.branch_ops
+import client.src.operations.enumerated_lemma_ops
+import client.src.operations.grammar_ops
+import client.src.operations.object_ops
+import client.src.operations.phrase_ops
+import client.src.operations.routine_ops
+import client.src.operations.state_ops
+import client.src.operations.verb_ops
+
