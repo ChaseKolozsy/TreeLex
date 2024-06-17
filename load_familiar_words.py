8223802e38eb9bb52a3ef7822e4818ebcb1892f6
Chase Kolozsy <chasekolozsy@gmail.com>
2024-06-17 14:50:08 -0600

    has load_familiar words take commandline arguments for filepath, language, and native language

--- load_familiar_words.py
+++ load_familiar_words.py
@@ -3,24 +3,22 @@ from pathlib import Path
 import sys
 import logging
 
-def process_text_file(file_path):
-    current_dir = Path.cwd()
-    data_dir = current_dir / "data"
-    if not data_dir.exists():
-        data_dir.mkdir(parents=True)
-    logging.info(f"data_dir: {data_dir}")
-    list_filepath = data_dir / "list.txt"
+def process_text_file(file_path, language="Hungarian", native_language="English"):
+    list_filepath = file_path
+    logging.info(f"list_filepath: {list_filepath}")
 
     matcher = Matcher(
-        language="Hungarian",
-        native_language="English",
+        language=language,
+        native_language=native_language,
         list_filepath=list_filepath
     )
     matcher.run()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python run_definition_generator.py <path_to_text_file>")
+    if len(sys.argv) != 3:
+        print("Usage: python run_definition_generator.py <path_to_text_file> <language> <native_language>")
     else:
         text_file_path = sys.argv[1]
-        process_text_file(text_file_path)
+        language = sys.argv[2]
+        native_language = sys.argv[3]
+        process_text_file(text_file_path, language=language, native_language=native_language)
