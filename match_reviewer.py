c6f5b9f6d5203080a52d6c5c4b814bdf4c431c0a
Chase Kolozsy <chasekolozsy@gmail.com>
2024-06-21 16:46:05 -0600

    match_reviewer now works with anthropic

--- match_reviewer.py
+++ match_reviewer.py
@@ -1,10 +1,11 @@
-import openai
-import anthropic
 import json
 import logging
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 from api_clients import OpenAIClient, AnthropicClient
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class MatchReviewer:
     def __init__(self, language, native_language, api_type="anthropic", model="claude-3-haiku-20240307"):
@@ -12,6 +13,7 @@ class MatchReviewer:
         self.native_language = native_language
         self.model = model
         self.max_retries = 3
+        self.api_type = api_type
 
         if api_type.lower() == "openai":
             self.client = OpenAIClient(model)
@@ -23,6 +25,55 @@ class MatchReviewer:
         self.example_input = {
             "phrase": "The boy played with his top.",
             "base_lemma": "top",
+            "phrase_info": [
+                                {
+                                    "text": "The boy played with his top.",
+                                    "tokens": [
+                                        {
+                                            "deprel": "det",
+                                            "lemma": "the",
+                                            "pos": "DET",
+                                            "text": "The"
+                                        },
+                                        {
+                                            "deprel": "nsubj",
+                                            "lemma": "boy",
+                                            "pos": "NOUN",
+                                            "text": "boy"
+                                        },
+                                        {
+                                            "deprel": "root",
+                                            "lemma": "play",
+                                            "pos": "VERB",
+                                            "text": "played"
+                                        },
+                                        {
+                                            "deprel": "case",
+                                            "lemma": "with",
+                                            "pos": "ADP",
+                                            "text": "with"
+                                        },
+                                        {
+                                            "deprel": "nmod:poss",
+                                            "lemma": "his",
+                                            "pos": "PRON",
+                                            "text": "his"
+                                        },
+                                        {
+                                            "deprel": "obl",
+                                            "lemma": "top",
+                                            "pos": "NOUN",
+                                            "text": "top"
+                                        },
+                                        {
+                                            "deprel": "punct",
+                                            "lemma": ".",
+                                            "pos": "PUNCT",
+                                            "text": "."
+                                        }
+                                    ]
+                                }
+                            ],
             "definition": "A toy that can be spun and maintain its balance until it loses momentum"
         }
         self.example_output_False = {
@@ -34,19 +85,85 @@ class MatchReviewer:
         self.bad_input = {
             "phrase": "The man achieved top performance at his job.",
             "base_lemma": "top",
+            "phrase_info": [
+                                {
+                                    "text": "The man achieved top performance at his job.",
+                                    "tokens": [
+                                        {
+                                            "deprel": "det",
+                                            "lemma": "the",
+                                            "pos": "DET",
+                                            "text": "The"
+                                        },
+                                        {
+                                            "deprel": "nsubj",
+                                            "lemma": "man",
+                                            "pos": "NOUN",
+                                            "text": "man"
+                                        },
+                                        {
+                                            "deprel": "root",
+                                            "lemma": "achieve",
+                                            "pos": "VERB",
+                                            "text": "achieved"
+                                        },
+                                        {
+                                            "deprel": "amod",
+                                            "lemma": "top",
+                                            "pos": "ADJ",
+                                            "text": "top"
+                                        },
+                                        {
+                                            "deprel": "obj",
+                                            "lemma": "performance",
+                                            "pos": "NOUN",
+                                            "text": "performance"
+                                        },
+                                        {
+                                            "deprel": "case",
+                                            "lemma": "at",
+                                            "pos": "ADP",
+                                            "text": "at"
+                                        },
+                                        {
+                                            "deprel": "nmod:poss",
+                                            "lemma": "his",
+                                            "pos": "PRON",
+                                            "text": "his"
+                                        },
+                                        {
+                                            "deprel": "obl",
+                                            "lemma": "job",
+                                            "pos": "NOUN",
+                                            "text": "job"
+                                        },
+                                        {
+                                            "deprel": "punct",
+                                            "lemma": ".",
+                                            "pos": "PUNCT",
+                                            "text": "."
+                                        }
+                                    ]
+                                }
+                            ],
             "definition": "A toy that can be spun and maintain its balance until it loses momentum"
         }
         self.base_instructions = "You are a helpful assistant that verifies that the definition is correct for " \
             f"a given base_lemma. You will take a dictionary like this: {json.dumps(self.example_input, indent=4)} " \
             "You will use the phrase for context to determine whether or not the definiton matches the base lemma. " \
-            "You will output json with a single key: `Is_Correct` and the value will be a boolean: " \
+            "You will strictly output json with a single key: `Is_Correct` and the value will be a boolean: " \
             f"{json.dumps(self.example_output_False, indent=4)} or {json.dumps(self.example_output_True, indent=4)}"
 
+        self.system_message = self.base_instructions
         self.base_message = {
             "role": "system",
-            "content": f"{self.base_instructions}"
+            "content": self.system_message
         }
-        self.messages = [self.base_message]
+
+        if self.api_type == "openai":
+            self.messages = [self.base_message]
+        else:
+            self.messages = []
 
     def get_validation_schema(self):
         try:
@@ -67,14 +184,17 @@ class MatchReviewer:
         retries = 0
         self.messages.append({
             "role": "user",
-            "content": f"{json.dumps(match_to_validate, indent=4)}"
+            "content": json.dumps(match_to_validate, indent=4, ensure_ascii=False)
         })
         logging.info(f"Messages: {self.messages}")
         backup_messages = self.messages.copy()
 
         while retries < max_retries:
             try:
-                response_message = self.client.create_chat_completion(self.messages)
+                if self.api_type == "openai":
+                    response_message = json.loads(self.client.create_chat_completion(self.messages, system=None))
+                else:
+                    response_message = json.loads(self.client.create_chat_completion(self.messages, system=self.system_message))
                 try:
                     validate(instance=response_message, schema=self.get_validation_schema())
                     logging.info(f"\n\nresponse_message: {json.dumps(response_message, indent=4)}")
@@ -92,12 +212,15 @@ class MatchReviewer:
         return None
     
     def run(self, match_to_validate):
-        self.messages = [self.base_message]
+        if self.api_type == "openai":
+            self.messages = [self.base_message]
+        else:
+            self.messages = []
         return self.review_matches(match_to_validate)
 
 
 if __name__ == "__main__":
-    match_reviewer = MatchReviewer(language="English", native_language="English", api_type="openai")
+    match_reviewer = MatchReviewer(language="English", native_language="English")
     is_correct_bad = match_reviewer.run(match_to_validate=match_reviewer.bad_input)
     is_correct_good = match_reviewer.run(match_to_validate=match_reviewer.example_input)
 
